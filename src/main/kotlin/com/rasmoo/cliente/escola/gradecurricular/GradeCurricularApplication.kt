7b656428900528a907963927b9c35bc4788eecba
Eduardo Souza <eduardo.souza@ifood.com.br>
2021-08-07 17:21:08 -0300

    Criacao do projeto

---
+++ src/main/kotlin/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.kt
@@ -0,0 +1,11 @@
+package com.rasmoo.cliente.escola.gradecurricular
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GradeCurricularApplication
+
+fun main(args: Array<String>) {
+	runApplication<GradeCurricularApplication>(*args)
+}
